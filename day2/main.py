c1ba67bc17d4a64c52ac02ea0f4d5f6420190605
Bryson Smith <15smith.bryson@gmail.com>
2025-12-02 16:02:23 -0600

    day 2 part 2

--- day2/main.py
+++ day2/main.py
@@ -7,7 +7,10 @@ def is_duplicates(num: str) -> bool:
 
 
 def is_duplicates2(num: str) -> bool:
-    return num == "".join(repeat(num[: len(num) // 2], 2))
+    for i in range(2, len(num) + 1):
+        if num == "".join(repeat(num[: len(num) // i], i)):
+            return True
+    return False
 
 
 def part_1() -> None:
@@ -25,8 +28,23 @@ def part_1() -> None:
     print(count)
 
 
+def part_2() -> None:
+    count = 0
+    with open("input.txt", "r") as file:
+        ranges = file.read().split(",")
+        for m_range in ranges:
+            m = m_range.split("-")
+            i = int(m[0])
+            j = int(m[1])
+            for k in range(i, j + 1):
+                if is_duplicates2(str(k)):
+                    count += k
+    print()
+    print(count)
+
+
 if __name__ == "__main__":
-    part_1()
+    part_2()
     # print(is_duplicates("446446"))
     # print(is_duplicates("55"))
     # print(is_duplicates("123"))
